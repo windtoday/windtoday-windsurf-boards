c4283f6dc6ceacf02abcdb1385a1073ac4788a8d
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-11-07 22:58:43 +0100

    Add str-match

--- test/index.js
+++ test/index.js
@@ -5,19 +5,25 @@ require('should')
 
 describe('directory', function () {
   it('nothing to detect', function () {
-    directory('').should.be.eql({})
+    const { board } = directory('')
+    board.should.be.eql({})
   })
 
   it('only detect brand', function () {
-    directory('starboard').should.be.eql({
+    const { board } = directory('starboard')
+    board.should.be.eql({
       brand: 'Starboard'
     })
   })
 
   it('detect brand and model', function () {
-    directory('starboard futura').should.be.eql({
+    const {board, output} = directory('vendo tabla starboard futura')
+
+    board.should.be.eql({
       brand: 'Starboard',
       model: 'Futura'
     })
+
+    output.should.be.equal('vendo tabla  ')
   })
 })
