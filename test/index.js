7428132ba8345c0e931e898cc54ad5e6899e871d
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-11-08 15:26:19 +0100

    Standarize interface

--- test/index.js
+++ test/index.js
@@ -4,26 +4,37 @@ var directory = require('..')
 require('should')
 
 describe('directory', function () {
-  it('nothing to detect', function () {
-    const { board } = directory('')
-    board.should.be.eql({})
-  })
+  describe('detection', function () {
+    it('nothing to detect', function () {
+      const { data } = directory('')
+      data.should.be.eql({})
+    })
 
-  it('only detect brand', function () {
-    const { board } = directory('starboard')
-    board.should.be.eql({
-      brand: 'Starboard'
+    it('only detect brand', function () {
+      const { data } = directory('starboard')
+      data.should.be.eql({
+        brand: 'Starboard'
+      })
+    })
+
+    it('detect brand and model', function () {
+      const {data} = directory('vendo tabla starboard futura')
+      data.should.be.eql({
+        brand: 'Starboard',
+        model: 'Futura'
+      })
     })
   })
 
-  it('detect brand and model', function () {
-    const {board, output} = directory('vendo tabla starboard futura')
-
-    board.should.be.eql({
-      brand: 'Starboard',
-      model: 'Futura'
+  describe('output', function () {
+    it('remove brand', function () {
+      const { output } = directory('vendo tabla starboard')
+      output.should.be.equal('vendo tabla ')
     })
 
-    output.should.be.equal('vendo tabla  ')
+    it('remove brand and model', function () {
+      const { output } = directory('vendo tabla starboard futura')
+      output.should.be.equal('vendo tabla  ')
+    })
   })
 })
